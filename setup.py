af29505922fb83c011f2dabe5975881406bf5b10
Angel Cardenas <Rek Dev>
2023-11-17 10:59:21 -0600

    Added setup.py file.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="esgrammar",
+    version="0.1.1",
+    packages=find_packages(),
+    setup_requires=[
+        "setuptools"
+    ]
+)
